8d1a666205cca0fb08f08da1b71135e347ba972f
Ritik <raj76941@gmail.com>
2024-10-18 16:51:18 +0530

    feat: used fieldArray for members

--- src/components/steps/Step2.tsx
+++ src/components/steps/Step2.tsx
@@ -1,93 +1,80 @@
-import { useForm } from "react-hook-form";
+import { useForm, useFieldArray } from "react-hook-form";
 import { z } from "zod";
 import { useStepForm } from "../../hooks/FormContext";
 import { Alert, Box, Button, Stack, TextField } from "@mui/material";
-import { useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 
-import { v4 as uuidv4 } from "uuid";
-
+// Define the schema for validation using Zod
 const step2Schema = z.object({
     members: z.array(
         z.object({
-            relation: z.string().min(5, "Must be min 5 character"),
+            relation: z.string().min(5, "Must be at least 5 characters"),
             name: z
                 .string()
-                .max(30, "Must be less than 30 character")
-                .min(4, "Must be min 4 character"),
+                .max(30, "Must be less than 30 characters")
+                .min(4, "Must be at least 4 characters"),
         })
     ),
 });
 
+// Infer types from the schema
 type Step2SchemaType = z.infer<typeof step2Schema>;
 
 export default function Step2() {
     const { changeStep, addToForm, formValues } = useStepForm();
 
-    const [totalInputs, setTotalInputs] = useState(
-        formValues["step2"].members.length || 1
-    );
+    console.log({ formValues });
 
     const {
-        register,
         handleSubmit,
-        setValue,
-        getValues,
+        register,
+        control,
         formState: { errors },
-    } = useForm({
+    } = useForm<Step2SchemaType>({
         resolver: zodResolver(step2Schema),
         defaultValues: {
-            ...formValues["step2"],
+            members:
+                formValues["step2"]?.members.length > 0
+                    ? formValues["step2"]?.members
+                    : [{ relation: "", name: "" }],
         },
+    });
+
+    const { fields, append, remove } = useFieldArray({
+        control,
+        name: "members",
     });
 
     const onSubmit = (value: Step2SchemaType) => {
         console.log(value);
-
         addToForm({ step2: value });
-
         changeStep(1);
-    };
-
-    const removeMember = (index: number) => {
-        const currentValue = getValues();
-
-        const newMembers = currentValue.members.filter((_, i) => index !== i);
-        setValue("members", newMembers);
-
-        setTotalInputs((prev) => prev - 1);
     };
 
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
-            <Box
-                sx={{
-                    marginBlock: "40px",
-                    marginInline: "auto",
-                }}
-            >
+            <Box sx={{ marginBlock: "40px", marginInline: "auto" }}>
                 <Stack spacing={2}>
                     <Stack
                         spacing={3}
                         justifyContent={"center"}
                         alignItems={"center"}
                     >
-                        {Array.from({ length: totalInputs }, (_, index) => (
-                            <Stack spacing={2} key={uuidv4()} direction={"row"}>
+                        {fields.map((item, index) => (
+                            <Stack spacing={2} key={item.id} direction={"row"}>
                                 <Stack spacing={1}>
                                     <TextField
                                         {...register(
                                             `members.${index}.relation`
                                         )}
-                                        id="outlined-basic"
                                         label="Relation"
                                         variant="outlined"
                                     />
-                                    {errors.members?.[index] && (
+                                    {errors.members?.[index]?.relation && (
                                         <Alert severity="error">
                                             {
-                                                errors.members?.[index].relation
-                                                    ?.message
+                                                errors.members[index].relation
+                                                    .message
                                             }
                                         </Alert>
                                     )}
@@ -95,30 +82,26 @@ export default function Step2() {
                                 <Stack spacing={1}>
                                     <TextField
                                         {...register(`members.${index}.name`)}
-                                        id="outlined-basic"
                                         label="Name"
                                         variant="outlined"
                                     />
-                                    {errors.members?.[index] && (
+                                    {errors.members?.[index]?.name && (
                                         <Alert severity="error">
-                                            {
-                                                errors.members?.[index].name
-                                                    ?.message
-                                            }
+                                            {errors.members[index].name.message}
                                         </Alert>
                                     )}
                                 </Stack>
                                 <Button
                                     variant="outlined"
                                     sx={{ height: "fit-content" }}
-                                    onClick={() => removeMember(index)}
+                                    onClick={() => remove(index)}
                                 >
                                     Delete
                                 </Button>
                             </Stack>
                         ))}
                         <Button
-                            onClick={() => setTotalInputs((prev) => prev + 1)}
+                            onClick={() => append({ relation: "", name: "" })}
                             sx={{ width: "fit-content" }}
                         >
                             Add Member
